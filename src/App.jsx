1b0b0b072d94ecd4ddd9b9bf345ec94ab4a416ed
lucaslmb971 <lucaslmb971@gmail.com>
2024-05-28 15:39:24 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Guitar from "./components/Guitar"
+import Header from "./components/Header"
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+        <Guitar />
+        <Guitar />
+        <Guitar />
+        <Guitar />
+        <Guitar />
+        <Guitar />
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default App
