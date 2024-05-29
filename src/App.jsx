3d78789291cd6355396011954a36e779bf4254a6
lucaslmb971 <lucaslmb971@gmail.com>
2024-05-28 20:57:21 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-import Guitar from "./components/Guitar"
-import Header from "./components/Header"
+import { useState } from "react";
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+import { db } from "./data/db";
 
 function App() {
-  
+  const [data, setData] = useState(db)
+  console.log(data);
 
   return (
     <>
-    <Header/>
+      <Header />
 
-    <main className="container-xl mt-5">
+      <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-        <Guitar />
-        <Guitar />
-        <Guitar />
-        <Guitar />
-        <Guitar />
-        <Guitar />
+          <Guitar />
+          <Guitar />
+          <Guitar />
+          <Guitar />
+          <Guitar />
+          <Guitar />
         </div>
-    </main>
+      </main>
 
-
-    <footer className="bg-dark mt-5 py-5">
+      <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
         </div>
-    </footer>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
